aadee86d63b08915fa8e730d86e06f46f8ab677e
Emma <emma.cebada@gmail.com>
2021-01-12 17:10:20 +0100

    Add task 1 from the react + redux course

---
+++ tarea-1/my-template/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div id="cuadro">
+      <h1>Hola mundo!!</h1>
+      <h2>Soy Emma y estoy aquí para aprender mucho! :D</h2>
+    </div>
+  );
+}
+
+export default App;
